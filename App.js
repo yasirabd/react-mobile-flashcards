98ba4e2cbf80012cc03b9a2cebf179f4708dc7e3
yasirabd <sir.yasirabd@gmail.com>
2018-02-10 14:35:22 +0700

    tab add deck

--- App.js
+++ App.js
@@ -2,20 +2,38 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { StackNavigator } from 'react-navigation'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 import thunk from 'redux-thunk'
 import reducer from './reducers'
 import DeckList from './components/DeckList'
 import DeckDetails from './components/DeckDetails'
+import DeckNew from './components/DeckNew'
 import AppStatusBar from './components/AppStatusBar'
 import { primary } from './utils/colors'
 
 const middlewares = [thunk]
 const store = createStore(reducer, applyMiddleware(...middlewares))
 
+const Tabs = TabNavigator(
+  {
+    DeckList: {
+      screen: DeckList,
+      navigationOptions: {
+        tabBarLabel: 'Decks',
+      }
+    },
+    DeckNew: {
+      screen: DeckNew,
+      navigationOptions: {
+        tabBarLabel: 'Add Deck'
+      }
+    }
+  }
+)
+
 const MainNavigator = StackNavigator({
   Home: {
-    screen: DeckList,
+    screen: Tabs,
   },
   DeckDetails: {
     screen: DeckDetails,
---
+++ components/DeckNew.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class DeckNew extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Add New Decks</Text>
+      </View>
+    )
+  }
+}
+
+export default DeckNew
